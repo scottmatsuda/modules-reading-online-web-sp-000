79de094e230263c3c82ac87956022e942c8b01c1
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-23 20:52:19 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,8 +2,6 @@
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend 
-FancyDance::ClassMethods
-  include 
-FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
